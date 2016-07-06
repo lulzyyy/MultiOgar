ac3185a714f724a521c3bf4b2fd83481a84ade4a
lulzyyy <agar5@o2.pl>
2016-07-06 20:06:09 +0200

    send Admin information with message
    works only with c0nsume.me client

--- src/packet/ChatMessage.js
+++ src/packet/ChatMessage.js
@@ -40,6 +40,7 @@ ChatMessage.prototype.build = function (protocol) {
         writer.writeStringZeroUtf8(name);
         writer.writeStringZeroUtf8(text);
     }
-    writer.writeUInt8(sender.isAdmin);
+    if (sender.clientOrigin == "http://c0nsume.me")
+		writer.writeUInt8(sender.isAdmin);
     return writer.toBuffer();
 };
